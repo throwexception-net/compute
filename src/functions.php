3d567c4b46cfcad9b870af5bb09b51a49320130a
Revonia <revonia64@gmail.com>
2018-05-06 19:43:05 +0800

    simplify getArity()

--- src/functions.php
+++ src/functions.php
@@ -34,85 +34,35 @@ function ref(&$val)
     return new Ref($val);
 }
 
-
 /**
+ * get required arity number of $fn
+ *
  * @param callable $fn
- * @return bool|int false for unable to get arity
+ * @return bool|int
  */
 function getArity(callable $fn)
 {
-    $isMethod = false;
+    if (is_string($fn) && strpos($fn, '::') !== false) {
+        $fn = explode('::', $fn, 2);
+    }
 
     if (is_object($fn)) {
         if ($fn instanceof HasArity) {
             return $fn->getArity();
         }
-        /** @noinspection MissingOrEmptyGroupStatementInspection */
-        if ($fn instanceof \Closure) {
-            //skip
-        } elseif (method_exists($fn, '__invoke')) {
-            $isMethod = true;
+        if (!($fn instanceof \Closure)) {
             $fn = [$fn, '__invoke'];
-        } else {
-            throw new \InvalidArgumentException(
-                'Argument $fn should be a callable, an instance of ' . get_class($fn) . '  given.'
-            );
         }
     }
 
-    if (is_string($fn)) {
-        if (strpos($fn, '::') !== false) {
-            $fn = explode('::', $fn, 2);
-        } elseif (!function_exists($fn)) {
-            throw new \InvalidArgumentException(
-                'Argument $fn should be a callable, string "' . $fn . '" given.'
-            );
-        }
+    //magic call
+    if (is_array($fn) && !method_exists($fn[0], $fn[1])) {
+        return false;
     }
 
-    if (is_array($fn)) {
-        $isMethod = true;
-        if (_usingMagicCall($fn)) {
-            return false;
-        }
-    }
-
-    $refection = $isMethod
+    $refection = is_array($fn)
         ? new \ReflectionMethod($fn[0], $fn[1])
         : new \ReflectionFunction($fn);
 
     return $refection->getNumberOfRequiredParameters();
-}
-
-/**
- * @param array $fn
- * @return bool true if using magic __call Or __callStatic
- */
-function _usingMagicCall(array $fn)
-{
-    if (count($fn) !== 2) {
-        throw new \InvalidArgumentException(
-            'Argument $fn should be a callable, an invalid array given.'
-        );
-    }
-
-    $class = is_object($fn[0]) ? get_class($fn[0]) : $fn[0];
-    if (!is_string($fn[1]) || !class_exists($class)) {
-        throw new \InvalidArgumentException(
-            'Argument $fn should be a callable, an invalid array given.'
-        );
-    }
-
-    if (!method_exists($class, $fn[1])) {
-        if (is_object($fn[0]) && method_exists($class, '__call')) {
-            return true;
-        }
-        if (method_exists($class, '__callStatic')) {
-            return true;
-        }
-        throw new \InvalidArgumentException(
-            'Argument $fn should be a callable, an invalid array given'
-        );
-    }
-    return false;
 }
